f26ff574e2b1641d0ca0ee903d1df90feca085c6
leobao <1baoleo@gmail.com>
2019-03-14 11:44:05 -0400

    camera control

--- Assets/Scripts/FollowPlayer.cs
+++ Assets/Scripts/FollowPlayer.cs
@@ -5,16 +5,27 @@ using UnityEngine;
 public class FollowPlayer : MonoBehaviour
 {
     public GameObject followTarget;
-
+    public float speed;
     public Vector3 offset;
+
+    private Vector3 prev;
 
     void Start()
     {
         offset = transform.position - followTarget.transform.position;
+        prev = followTarget.transform.position;
     }
-    // Update is called once per frame
-    void LateUpdate()
-    {
-        transform.position = followTarget.transform.position + offset;
+
+    void LateUpdate() {
+        Vector3 cursorOffset = new Vector3(Input.GetAxisRaw("Mouse X") * Time.deltaTime * speed,
+             Input.GetAxisRaw("Mouse Y") * Time.deltaTime * speed, 0f);
+        cursorOffset += (followTarget.transform.position - prev);
+        /*Camera cam = gameObject.GetComponent<Camera>();
+        cursorOffset.x = ((Screen.width / 2) - (Input.mousePosition.x));
+        cursorOffset.y = ((Screen.height / 2) - (Input.mousePosition.y));
+        Vector3 finalOffset = cam.ScreenToWorldPoint(cursorOffset);
+        Debug.Log(finalOffset);*/
+        transform.position += cursorOffset;
+        prev = followTarget.transform.position;
     }
 }
---
+++ Assets/Scripts/Initialize.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Initialize : MonoBehaviour
+{
+    public Texture2D crosshair;
+    private CursorMode cursorMode = CursorMode.Auto;
+
+        void Start()
+    {
+        Cursor.visible = true;
+        Cursor.SetCursor(crosshair, Vector2.zero, cursorMode);
+    }
+
+}
