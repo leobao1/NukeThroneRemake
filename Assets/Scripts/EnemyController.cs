d47052c1d066125a6df5894f722dbdeac08f82a5
leobao <1baoleo@gmail.com>
2019-05-15 16:31:45 -0400

    started creating enemies, created vision script

---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    public float speed;
+    public Transform player;
+
+    private Rigidbody2D rb;
+
+    float moveHorizontal;
+    float moveVertical;
+
+    Vector3 playerPos;
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/InRange.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InRange : MonoBehaviour
+{
+    public int dist;
+    public Transform player;
+
+    bool sawPlayer = false;
+    int layerMask;
+
+    private void Start() {
+        int layerNum1 = LayerMask.NameToLayer("Wall");
+
+        if (layerNum1 == -1) {
+            Debug.Log("invalid layer name");
+        }
+
+        layerMask = (1 << layerNum1);
+    }
+
+    private void OnTriggerStay2D(Collider2D collision) {
+        if (collision.gameObject.CompareTag("Player")) {
+            Vector3 curr = gameObject.transform.position;
+            Vector3 direc = player.position - curr;
+            sawPlayer = Physics2D.Raycast(curr, direc, direc.magnitude, layerMask);
+           // RaycastHit2D ray = Physics2D.Raycast(curr, direc, direc.magnitude, layerMask);
+            Debug.DrawRay(curr, direc, Color.red, 0.05f, true);
+            Debug.Log(sawPlayer);
+        }
+    }
+
+    public bool getVision() {
+        return sawPlayer;
+    }
+}
